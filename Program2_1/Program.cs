bf48994271173eeeca6da3555c01d18621c172ca
Rekhtina Lyudmila <Rehtina520@mail.ru>
2022-12-09 16:50:47 +0700

    Задача1

---
+++ Program2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Enter three_digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Second digit of the number -> "+stringNumber[1]);
